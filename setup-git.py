111c234068fca6c2b353d11242d068b53e5b3829
Avihu Turzion <avihu@bugatone.com>
2017-06-19 05:15:45 +0300

    Added SSH configuration

--- setup-git.py
+++ setup-git.py
@@ -38,6 +38,24 @@ class Wizard:
     def ask_user_for_email(self):
         return raw_input("Please enter your GitHub user email: ").strip()
 
+    def ask_if_user_wants_to_setup_ssh_key(self):
+        return raw_input("Would you like to generate an SSH key? [Y/n]: ") != 'n'
+
+    def print_ssh_keygen_instructions(self):
+        print('')
+        print("** When prompted to enter a file path to save your SSH key just click Enter.")
+        print("** When prompted for a passphrase, it's not the your user password. You can leave it empty, but make sure to remember the passphrase if you wish to set it.")
+
+    def print_public_ssh_key_with_instructions(self, public_ssh_key):
+        print("\n==========================================================================")
+        print("Follow instructions to complete ssh connection:")
+        print("Copy the key below (everything between the triple quotes) and go to to the link: https://github.com/settings/keys\n")
+        print("\"\"\"")
+        print("%s" % public_ssh_key)
+        print("\"\"\"\n")
+        print("There click the 'New SSH key' button and paste it in the designated text field.")
+        print("The Title requested is just for tracking. You can enter 'Work Desktop'.")
+
 
 class GitConfig:
     def get_user_name(self):
@@ -69,6 +87,20 @@ class GitConfig:
         subprocess.call(['git', 'config', '--global', 'user.email', "%s" % user_email])
         pass
 
+    def configure_ssh(self, withWizard):
+        wizard = withWizard
+        wizard.print_ssh_keygen_instructions()
+        self._generate_ssh_keys()
+        public_ssh_key = self._get_public_ssh_key()
+        wizard.print_public_ssh_key_with_instructions(public_ssh_key)
+
+    def _generate_ssh_keys(self):
+        subprocess.call(['ssh-keygen', '-t', 'rsa', '-b', '4096', '-C', '%s' % self.get_user_email()])
+
+    def _get_public_ssh_key(self):
+        public_ssh_file = open(os.path.join(os.sep, 'home', os.environ['USER'], '.ssh', 'id_rsa.pub'))
+        return '\n'.join(public_ssh_file.readlines())
+
 
 if __name__ == "__main__":
     wizard = Wizard()
@@ -88,6 +120,9 @@ if __name__ == "__main__":
         else:
             if wizard.ask_if_user_wants_to_keep_current_email(gitConfig.get_user_email()) == DECISION.CHANGE:
                 gitConfig.configure_user_email(withWizard=wizard)
+
+        if wizard.ask_if_user_wants_to_setup_ssh_key():
+            gitConfig.configure_ssh(withWizard=wizard)
 
         wizard.say_goodbye()
 
